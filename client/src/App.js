0f3d44b2049a79247b9671c07757cae30730dfcf
Patel Dharmesh <pateldharmesh8262@gmail.com>
2024-06-29 02:04:05 +0530

    Feat: Added route and context for the invoice data

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 // src/App.js
-import React from 'react';
-import InvoiceForm from './InvoiceForm';
-import { CssBaseline, Container } from '@mui/material';
+import React, { useState } from "react";
+import InvoiceForm from "./InvoiceForm";
+import { CssBaseline, Container } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Invoice from "./Invoice";
+import InvoiceContext from "./utils/invoiceContext";
 
 function App() {
+  const [image, setImage] = useState();
+  const [invoiceData, setInvoiceData] = useState({});
+  console.log(invoiceData);
+
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container>
-        <InvoiceForm />
-      </Container>
+      <InvoiceContext.Provider value={{ invoiceData, setInvoiceData }}>
+        <Container>
+          <Router>
+            <Routes>
+              <Route path="/" element={<InvoiceForm />} />
+              <Route
+                path="/invoice"
+                element={<Invoice data={{ ...invoiceData }} />}
+              />
+            </Routes>
+          </Router>
+        </Container>
+      </InvoiceContext.Provider>
     </React.Fragment>
   );
 }
